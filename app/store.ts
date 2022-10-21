198215746c348f1f121d780317672e383d23266b
yuzheng14 <422450117@qq.com>
2022-10-21 23:02:36 +0800

    login

--- app/store.ts
+++ app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "../features/login-slice";
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        login: loginReducer
+    }
 })
 
 export default store
